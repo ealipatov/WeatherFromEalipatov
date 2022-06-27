6dc0615880475dc8d018f5e057235adfaefe7d5c
Evgeniy Alipatov <ealipatov@gmail.com>
2022-06-27 10:47:49 +0300

    Урок 2. Подключили ViewBinding, проверили как работает.

--- app/src/main/java/by/ealipatov/kotlin/weatherfromealipatov/MainActivity.kt
+++ app/src/main/java/by/ealipatov/kotlin/weatherfromealipatov/MainActivity.kt
@@ -2,10 +2,17 @@ package by.ealipatov.kotlin.weatherfromealipatov
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.ealipatov.kotlin.weatherfromealipatov.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.textOnScreen.text = getString(R.string.test_text)
     }
 }
