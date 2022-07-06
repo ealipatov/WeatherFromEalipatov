0b0007ce849d9b68fe7f039fcea60ab063f8ecac
Evgeniy Alipatov <ealipatov@gmail.com>
2022-07-06 16:25:17 +0300

    Урок 5. Настроили отображение ввененного url (выполнение запроса в отдельном потоке)

--- app/src/main/java/by/ealipatov/kotlin/weatherfromealipatov/view/WebViewFragment.kt
+++ app/src/main/java/by/ealipatov/kotlin/weatherfromealipatov/view/WebViewFragment.kt
@@ -1,11 +1,20 @@
 package by.ealipatov.kotlin.weatherfromealipatov.view
 
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import by.ealipatov.kotlin.weatherfromealipatov.databinding.FragmentWebViewBinding
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.stream.Collectors
 
 class WebViewFragment: Fragment() {
 
@@ -27,5 +36,37 @@ class WebViewFragment: Fragment() {
     ): View {
         _binding = FragmentWebViewBinding.inflate(inflater)
         return binding.root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.ok.setOnClickListener {
+            binding.url.text.let { textURL ->
+                val url = URL(textURL.toString())
+                var myConnection: HttpURLConnection? = null
+                val handler = Handler(Looper.myLooper()!!)
+
+                myConnection = url.openConnection() as HttpURLConnection
+                myConnection.connectTimeout = 5000
+
+                Thread{
+                    val reader = BufferedReader(InputStreamReader(myConnection.inputStream))
+                    val result = getLines(reader)
+
+                    handler.post {
+                            binding.webView.loadDataWithBaseURL(null,result,"text/html; charset=utf-8","utf-8",null)
+                    }
+
+                }.start()
+            }
+        }
+    }
+
+    //Функция из методички
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun getLines(reader: BufferedReader): String{
+        return reader.lines().collect(Collectors.joining("\n"))
     }
 }
