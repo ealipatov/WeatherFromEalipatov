3e271311528bba8ec802f8c0a0843670f1133041
Evgeniy Alipatov <ealipatov@gmail.com>
2022-07-11 08:33:04 +0300

    Урок 6. Создали фрагмент для проверки работы потоков. Создали меню и добавили вызов в MainA MainActivity.kt

--- app/src/main/java/by/ealipatov/kotlin/weatherfromealipatov/MainActivity.kt
+++ app/src/main/java/by/ealipatov/kotlin/weatherfromealipatov/MainActivity.kt
@@ -2,7 +2,10 @@ package by.ealipatov.kotlin.weatherfromealipatov
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import by.ealipatov.kotlin.weatherfromealipatov.databinding.ActivityMainBinding
+import by.ealipatov.kotlin.weatherfromealipatov.lesson06.ThreadsFragment
 import by.ealipatov.kotlin.weatherfromealipatov.view.WeatherListFragment
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +20,26 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, WeatherListFragment.newInstance()).commit()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.threads -> {
+                supportFragmentManager.apply {
+                    beginTransaction()
+                        .add(R.id.container, ThreadsFragment())
+                        .addToBackStack("")
+                        .commitAllowingStateLoss()
+                }
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
---
+++ app/src/main/java/by/ealipatov/kotlin/weatherfromealipatov/lesson06/ThreadsFragment.kt
@@ -0,0 +1,31 @@
+package by.ealipatov.kotlin.weatherfromealipatov.lesson06
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import by.ealipatov.kotlin.weatherfromealipatov.databinding.FragmentThreadsBinding
+
+class ThreadsFragment: Fragment() {
+
+    private var _binding: FragmentThreadsBinding? = null
+    private val binding: FragmentThreadsBinding
+        get() {
+            return _binding!!
+        }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentThreadsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
