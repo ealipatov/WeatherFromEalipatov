a66f708895f2f90cd69349e927369537c96d473f
Evgeniy Alipatov <ealipatov@gmail.com>
2022-07-11 11:41:25 +0300

    Урок 6. Оставили в меню вызов фрагмента с описанием программы (заглушка). Создали файл сервиса для дальнейшей обработки сетевого запроса погоды через него.

---
+++ app/src/main/java/by/ealipatov/kotlin/weatherfromealipatov/view/LoadWeatherService.kt
@@ -0,0 +1,14 @@
+package by.ealipatov.kotlin.weatherfromealipatov.view
+
+import android.app.IntentService
+import android.content.Intent
+
+class LoadWeatherService : IntentService("LoadWeatherService") {
+
+    @Deprecated("Deprecated in Java")
+    override fun onHandleIntent(intent: Intent?) {
+
+    }
+
+
+}
