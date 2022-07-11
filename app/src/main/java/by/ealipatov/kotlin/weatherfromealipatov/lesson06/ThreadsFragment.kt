8d3c4cfadfae014062ba5bd369824c8bf350ec5b
Evgeniy Alipatov <ealipatov@gmail.com>
2022-07-11 08:57:46 +0300

    Урок 6. Добавим код из урока и проверим работу потоков.

--- app/src/main/java/by/ealipatov/kotlin/weatherfromealipatov/lesson06/ThreadsFragment.kt
+++ app/src/main/java/by/ealipatov/kotlin/weatherfromealipatov/lesson06/ThreadsFragment.kt
@@ -1,9 +1,13 @@
 package by.ealipatov.kotlin.weatherfromealipatov.lesson06
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import by.ealipatov.kotlin.weatherfromealipatov.databinding.FragmentThreadsBinding
 
@@ -28,4 +32,62 @@ class ThreadsFragment: Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Log.d("***","${Thread.currentThread()}")
+        var counter =0
+
+        binding.button.setOnClickListener {
+            Thread{
+                Thread.sleep(200L)
+                Handler(Looper.getMainLooper()).post(Runnable {
+                    binding.textView.text= hardWork()
+                    binding.button.textSize =10f
+                    binding.mainContainer.addView(TextView(requireContext()).apply {
+                        text ="${counter++}"
+                        textSize =20f
+                    })
+                })
+            }.start()
+        }
+
+        val mThread =MyThread()
+        mThread.start()
+        binding.button2.setOnClickListener {
+            mThread.handler?.let {
+                it.post{
+                    Log.d("***","${Thread.currentThread()} $counter")
+                    hardWork()
+                    Handler(Looper.getMainLooper()).post(Runnable {
+                        binding.textView2.text=""
+                        binding.button2.textSize =10f
+                        binding.mainContainer.addView(TextView(requireContext()).apply {
+                            text ="${counter++}"
+                            textSize =20f
+                        })
+                    })
+                }
+            }
+        }
+    }
+
+    class MyThread:Thread(){
+        var handler: Handler?=null
+        override fun run() {
+            Looper.prepare()
+            handler = Handler()
+            Looper.loop()
+        }
+    }
+
+    private fun hardWork():String{
+        Thread.sleep(200L)
+        return hardWork2()
+    }
+
+    private fun hardWork2():String{
+        Thread.sleep(200L)
+        return "Получилось"
+    }
 }
