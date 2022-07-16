dc96e561f38b805e85ca1edfa4645b0f72fdacbe
Evgeniy Alipatov <ealipatov@gmail.com>
2022-07-16 23:59:34 +0300

    Урок 7. Создали интерфейс для работы с базой данных (таблицей)

---
+++ app/src/main/java/by/ealipatov/kotlin/weatherfromealipatov/model/room/HistoryWeatherDAO.kt
@@ -0,0 +1,19 @@
+package by.ealipatov.kotlin.weatherfromealipatov.model.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface HistoryWeatherDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert (historyWeatherEntity: HistoryWeatherEntity)
+
+    @Query("SELECT * FROM history_weather_entity_table WHERE lat= :sLat AND lon= :sLon") //* - прочитать все поля, можно отдельное поле
+    fun getHistoryWeatherByCoordinates(sLat: Double, sLon: Double): List<HistoryWeatherEntity>
+
+    //Получить все записи (без условий отбора)
+    @Query("SELECT * FROM history_weather_entity_table")
+    fun getHistoryWeatherAll(): List<HistoryWeatherEntity>
+}
