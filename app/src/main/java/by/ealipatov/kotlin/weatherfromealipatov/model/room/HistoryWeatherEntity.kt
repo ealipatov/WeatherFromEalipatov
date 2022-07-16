ed5467a2ea2f0348c494ce90f1faf0c0ded7b392
Evgeniy Alipatov <ealipatov@gmail.com>
2022-07-16 22:51:12 +0300

    Урок 7. Подключили библиотеку Room. Создали датакласс HistoryWeatherEntity, который в последствии сконвертируется в таблицу.

---
+++ app/src/main/java/by/ealipatov/kotlin/weatherfromealipatov/model/room/HistoryWeatherEntity.kt
@@ -0,0 +1,17 @@
+package by.ealipatov.kotlin.weatherfromealipatov.model.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "history_weather_entity_table")
+data class HistoryWeatherEntity (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val country: String,
+    val name: String,
+    val lat: Double,
+    val lon: Double,
+    var temperature: Int,
+    var feelsLike: Int,
+    var condition: String
+)
