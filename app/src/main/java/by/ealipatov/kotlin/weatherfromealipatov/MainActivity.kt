7fdcec01e01e47323f6bd62b55579a72eb12aa2f
Evgeniy Alipatov <ealipatov@gmail.com>
2022-07-30 08:51:24 +0300

    Урок 11. Создали функцию отправки пуш-уведомления из приложения.

--- app/src/main/java/by/ealipatov/kotlin/weatherfromealipatov/MainActivity.kt
+++ app/src/main/java/by/ealipatov/kotlin/weatherfromealipatov/MainActivity.kt
@@ -1,14 +1,19 @@
 package by.ealipatov.kotlin.weatherfromealipatov
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.ConnectivityManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import by.ealipatov.kotlin.weatherfromealipatov.databinding.ActivityMainBinding
+import by.ealipatov.kotlin.weatherfromealipatov.utils.*
 import by.ealipatov.kotlin.weatherfromealipatov.view.citylist.CityListFragment
 
 class MainActivity : AppCompatActivity() {
@@ -25,8 +30,11 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, CityListFragment.newInstance()).commit()
         }
 
+        @Suppress("DEPRECATION")
         val filter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
         registerReceiver(networkStateReceiver, filter)
+
+        pushNotification("Погода сегодня", "Какие-то данные о погоде")
     }
 
     private var networkStateReceiver: BroadcastReceiver = object : BroadcastReceiver() {
@@ -34,10 +42,33 @@ class MainActivity : AppCompatActivity() {
             val noConnectivity =
                 intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false)
             if (!noConnectivity) {
-                Toast.makeText(context,"Connection found", Toast.LENGTH_LONG).show()
+                Toast.makeText(context, "Connection found", Toast.LENGTH_LONG).show()
             } else {
                 Toast.makeText(context, "Connection lost", Toast.LENGTH_LONG).show()
             }
         }
+    }
+
+    private fun pushNotification(title: String, message: String) {
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notification = NotificationCompat.Builder(this, CHANNEL_WEATHER_ID)
+        notification.apply {
+            setContentTitle(title)
+            setContentText(message)
+            setSmallIcon(R.drawable.ic_baseline_beach_access_24)
+            priority = NotificationCompat.PRIORITY_MAX
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelWeather = NotificationChannel(
+                CHANNEL_WEATHER_ID,
+                CHANNEL_WEATHER_NAME,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            channelWeather.description = "Канал уведомлений о погоде"
+            notificationManager.createNotificationChannel(channelWeather)
+        }
+        notificationManager.notify(NOTIFICATION_WEATHER_ID, notification.build())
     }
 }
